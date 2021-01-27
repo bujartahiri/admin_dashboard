42257859fe64d1a2d44d50c7a098fd4eded3d29d
Bujar Tahiri <bujaar.tahiri@gmail.com>
2021-01-27 04:14:20 +0100

    Dashboard layout and created the sidebar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
-    Home
+    <Dashboard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Dashboard from '@/components/Dashboard.vue'
 export default {
   name: "Home",
   components: {
-    //
+    Dashboard
   }
 };
 </script>
