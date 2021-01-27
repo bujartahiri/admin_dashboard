5b2792bae2e770a333e06473da1d8ae83228ec48
Bujar Tahiri <bujaar.tahiri@gmail.com>
2021-01-27 06:36:13 +0100

    Small css details added to login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="bg-blue-100">
-    <div class="container mx-auto h-screen">
-    <div class="login">
-      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-        <div class="mb-4">
-          <label class="block text-grey-darker text-sm font-bold mb-2" for="Email">
-            Email
-          </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email"
-            type="text" placeholder="Email">
-        </div>
-        <div class="mb-6">
-          <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-            Password
-          </label>
-          <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-            id="password" type="password" placeholder="******************">
-        </div>
-        <div class="flex items-center justify-between">
-          <button class="bg-blue-600 hover:bg-blue-light text-white font-semibold py-2 px-4 rounded">
-            Sign In
-          </button>
-        </div>
+  <div class="login-container bg-blue-200">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <div class="mb-4">
+        <label class="block text-grey-darkerfont-bold mb-2" for="Email">
+          Email
+        </label>
+        <input class="shadow focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email" type="text"
+          placeholder="Email">
+      </div>
+      <div class="mb-6">
+        <label class="block text-grey-darker  font-bold mb-2" for="password">
+          Password
+        </label>
+        <input class="shadow focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+          id="password" type="password" placeholder="********">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-600  focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 text-white font-semibold py-2 px-4 rounded">
+          Sign In
+        </button>
       </div>
     </div>
   </div>
-  </div>
+
 </template>
 
 <script>
@@ -35,9 +32,12 @@
 </script>
 
 <style scoped>
-  .login {
-    max-width: 400px;
-    margin: 0 auto;
-    padding-top: 300px;
+  .login-container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+
 </style>
