aaf2bb396cac211bfebeb4f5acfbabcd24081baa
Bujar Tahiri <bujaar.tahiri@gmail.com>
2021-01-29 07:47:14 +0100

    Auto-authentication

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false;
 
+const token = localStorage.getItem('user-token')
+if(token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
 new Vue({
   router,
   store,
